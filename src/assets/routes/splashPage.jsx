c0d9cf7aa18086ced565f51fc3db253a73a292ed
CottonLeSergal <cdbabtempa@outlook.com>
2024-10-13 19:45:58 -0400

    testing links 6

--- src/assets/routes/splashPage.jsx
+++ src/assets/routes/splashPage.jsx
@@ -96,7 +96,7 @@ function SplashPage() {
 
     const transformLayer = ({x, y}) => {
         // Define the parallax image location
-        const parallaxImageLoc = './images/splash/';
+        const parallaxImageLoc = './../assets/images/splash/';
         // Simulate the list of files (in real-life scenarios you might want to fetch these from the server)
         const listOfFiles = [
             'farMountains.webp',
@@ -179,7 +179,7 @@ function SplashPage() {
                         </div>
                     </div>
                     <div className="game-box">
-                        <img className="game-image" src="./assets/images/ProwlerDownloadImage.png"
+                        <img className="game-image" src="./src/assets/images/ProwlerDownloadImage.png"
                              alt="Prowler Download Image"/>
                         <div className="game-container">
                             <h3>Get the game on Itch.io!</h3>
