a4854a3ab1bdda5828f6733d31e2fd4191aace62
CottonLeSergal <cdbabtempa@outlook.com>
2024-10-13 08:34:19 -0400

    Restructured project files, recreated App.jsx and modified main.jsx to support App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
+import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
 import Crafting from "./routes/Crafting.jsx";
 import Walkthrough from "./routes/Walkthrough.jsx";
 import SplashPage from "./routes/splashPage.jsx";
+
 const App = () => {
     return (
         <Router>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<SplashPage/>}/>
-                    {/* Add more routes here if needed */}
-                    <Route path="/crafting" element={<Crafting/>}/>
-                    <Route path="/walkthrough" element={<Walkthrough/>}/>
-                </Routes>
-            </Router>
+            <Routes>
+                <Route path="/" element={<Navigate to="/splash"/>}/>
+                <Route path="/splash" element={<SplashPage/>}/>
+                <Route path="/crafting" element={<Crafting/>}/>
+                <Route path="/walkthrough" element={<Walkthrough/>}/>
+            </Routes>
         </Router>
     );
 };
