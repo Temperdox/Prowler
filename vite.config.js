39e259df93007ddbfdecef22c8ff5381684f826e
CottonLeSergal <cdbabtempa@outlook.com>
2024-10-13 20:15:50 -0400

    testing links 8

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // Ensures that assets are loaded relative to the current directory
+  base: '/', // Ensures that assets are loaded from the root
   plugins: [react()],
   resolve: {
     alias: {
