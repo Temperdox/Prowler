d0a12c3d973800feabd4cd35726754cb046c576e
CottonLeSergal <cdbabtempa@outlook.com>
2024-10-14 02:30:43 -0400

    testing links 18

--- src/assets/routes/splashPage.jsx
+++ src/assets/routes/splashPage.jsx
@@ -92,7 +92,7 @@ function SplashPage() {
     }, []);
 
     const transformLayer = ({x, y}) => {
-        const parallaxImageLoc = '../images/';
+        const parallaxImageLoc = '../assets/';
 
         const listOfFiles = [
             'farMountains.webp',
